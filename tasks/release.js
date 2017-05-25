fa828b7d55490096cde7c785937190fc23a12176
hankaibo <hankaibo@hotmail.com>
2017-05-25 16:39:32 +0800

    style:格式化代码
    用变量代码ANSI color codes

--- tasks/release.js
+++ tasks/release.js
@@ -3,16 +3,23 @@
 const exec = require('child_process').exec
 const packager = require('electron-packager')
 
+// ANSI color codes
+// http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
+let RED = '\x1b[31m'
+let YELLOW = '\x1b[33m'
+let BLUE = '\x1b[34m'
+let END = '\x1b[0m'
+
 if (process.env.PLATFORM_TARGET === 'clean') {
   require('del').sync(['builds/*', '!.gitkeep'])
-  console.log('\x1b[33m`builds` directory cleaned.\n\x1b[0m')
+  console.log(`${YELLOW}builds directory cleaned.\n${END}`)
 } else pack()
 
 /**
  * Build webpack in production
  */
-function pack () {
-  console.log('\x1b[33mBuilding webpack in production mode...\n\x1b[0m')
+function pack() {
+  console.log(`${YELLOW}Building webpack in production mode...\n${END}`)
   let pack = exec('npm run pack_ori:electron-packager')
 
   pack.stdout.on('data', data => console.log(data))
@@ -24,19 +31,19 @@ function pack () {
 /**
  * Use electron-packager to build electron app
  */
-function build () {
+function build() {
   let options = require('../config').building
 
-  console.log('\x1b[34mBuilding electron app(s)...\n\x1b[0m')
+  console.log(`${BLUE}Building electron app(s)...\n${END}`)
   packager(options, (err, appPaths) => {
-    if(err) {
-      console.error('\x1b[31mError from `electron-packager` when building app...\x1b[0m')
+    if (err) {
+      console.error(`${RED}Error from electron - packager when building app...${END}`)
       console.error(err)
     } else {
       console.log('Build(s) successful!')
       console.log(appPaths)
 
-      console.log('\n\x1b[34mDONE\n\x1b[0m')
+      console.log(`\n${BLUE}DONE\n${END}`)
     }
   })
 }
--- tasks/runner.js
+++ tasks/runner.js
@@ -7,26 +7,28 @@ const config = require('../config')
 const exec = require('child_process').exec
 const treeKill = require('tree-kill')
 
+// ANSI color codes
+// http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html
 let YELLOW = '\x1b[33m'
 let BLUE = '\x1b[34m'
 let END = '\x1b[0m'
 
 let isElectronOpen = false
 
-function format (command, data, color) {
+function format(command, data, color) {
   return color + command + END +
     '  ' + // Two space offset
     data.toString().trim().replace(/\n/g, '\n' + repeat(' ', command.length + 2)) +
     '\n'
 }
 
-function repeat (str, times) {
+function repeat(str, times) {
   return (new Array(times + 1)).join(str)
 }
 
 let children = []
 
-function run (command, color, name) {
+function run(command, color, name) {
   let child = exec(command)
 
   child.stdout.on('data', data => {
@@ -56,11 +58,11 @@ function run (command, color, name) {
   children.push(child)
 }
 
-function exit (code) {
+function exit(code) {
   children.forEach(child => {
     treeKill(child.pid)
   })
 }
 
 console.log(`${YELLOW}Starting webpack-dev-server...\n${END}`)
-run(`webpack-dev-server --inline --hot --colors --port ${config.port} --content-base app/dist`, YELLOW, 'webpack')
+run(`webpack-dev-server --inline --hot --colors --port ${config.port}`, YELLOW, 'webpack')
